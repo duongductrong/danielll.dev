cee2aef47fbdad252dc1f1e0fd1e12c75a570085
duongductrong <duongductrong06@gmail.com>
2024-07-15 22:27:08 +0700

    chore: remove transition in labs

--- src/app/(2024)/labs/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React, { PropsWithChildren } from "react";
-
-export interface LabsLayoutProps extends PropsWithChildren {}
-
-const LabsLayout = ({ children }: LabsLayoutProps) => {
-  return children;
-};
-
-export default LabsLayout;
