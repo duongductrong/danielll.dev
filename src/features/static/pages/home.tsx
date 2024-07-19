0a58b5312b765de5fd343c073630afe91764744d
duongductrong <duongductrong06@gmail.com>
2024-07-19 18:24:38 +0700

    feat: add drag & bounce damping to avatar

--- src/features/static/pages/home.tsx
+++ src/features/static/pages/home.tsx
@@ -48,6 +48,9 @@ const HomePage = (props: HomeProps) => {
           <MotionImage
             whileHover={{ scale: 1.2 }}
             whileTap={{ scale: 0.9 }}
+            drag
+            dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
+            dragTransition={{ bounceDamping: 10 }}
             src="/assets/peeps-avatar-alpha-transparent.png"
             width={80}
             height={80}
