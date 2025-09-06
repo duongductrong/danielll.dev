1c67e5e908b8918d5dbf94bd44f977cdd4b226fa
duongductrong <duongductrong06@gmail.com>
2025-09-06 11:15:04 +0700

    fix: update Timezone component to display local time in Vietnamese format

--- src/features/garden/components/timezone.tsx
+++ src/features/garden/components/timezone.tsx
@@ -9,10 +9,9 @@ const Timezone = ({ className, ...props }: TimezoneProps) => {
       <p className="text-xl font-semibold mb-1">Based in HCMC, Vietnam.</p>
       <p className="text-muted-foreground font-light">
         Local time:{" "}
-        {new Date().toLocaleTimeString("en-US", {
+        {new Date().toLocaleTimeString("vi-VN", {
           timeZone: "Asia/Ho_Chi_Minh",
           timeZoneName: "longOffset",
-          hour12: false,
         })}
       </p>
     </section>
