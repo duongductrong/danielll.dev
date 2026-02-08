a0c5cc581228ae67530013a074c7b64e85d6573e
duongductrong <duongductrong06@gmail.com>
2026-02-08 14:59:05 +0700

    fix: enable nitro plugin in Vite configuration for improved performance

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
-// import { nitro } from 'nitro/vite'
+import { nitro } from 'nitro/vite'
 
 const config = defineConfig({
   plugins: [
     devtools(),
-    // nitro(),
     // this is the plugin that enables path aliases
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
     tanstackStart(),
+    nitro(),
     viteReact(),
   ],
 })
